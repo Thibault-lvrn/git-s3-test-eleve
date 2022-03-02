d41c55f44ef6fa627b5c56043288295a90613975
thibault-lvrn <thibault.livran@etudiant.univ-reims.fr>
2022-03-02 09:20:42 +0100

    code php

---
+++ header.php
@@ -0,0 +1,23 @@
+<header>
+    <div class="logo">logo</div>
+    <div class="menu_div">
+        <div class="logo_menu"><label for="ch_menu" class="menu_lien"></label></div>
+        <input type="checkbox" id="ch_menu">
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="index.html">menu 1</a>
+                </li>
+                <li>
+                    <a href="index.html">menu 2</a>
+                </li>
+                <li>
+                    <a href="index.html">menu 3</a>
+                </li>
+                <li>
+                    <a href="index.html">menu 4</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+            require 'debut.php';
+            require 'header.php';
+?>
+    <main>
+        <div>
+            <div class="image"><img src="images/BTCG-Griffith_Holding_Sabre-OI.webp" alt=""></div>
+            <div class="image"><img src="images/guts_175.jpg" alt=""><div>
+        </div>
+    </main>
+<?php 
+    require 'footer.php';
+    require 'fin.php';
+?>
